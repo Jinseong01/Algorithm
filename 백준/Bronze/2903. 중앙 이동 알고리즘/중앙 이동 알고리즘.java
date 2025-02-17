f1e0396e778ee8ae78eea50a51a4a89ac668415b
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-02-18 02:46:02 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -3,12 +3,21 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-		br.close();
-        
-        // Math의 클래스의 pow()메서드를 사용
-        // Math.pow(거듭제곱할 숫자, 거듭제곱(몇제곱인지);
-        System.out.println((int)Math.pow(Math.pow(2, n) +1, 2));
+
+        // 초기 : 4
+        //  n = 1 -> 9 = 3^2 = (2+1)^2
+        // n = 2 -> 25 = 5^2 = (4+1)^2
+        // n = 3 -> 49 = 9^2 = (8+1)^2
+        // n = 4 -> ?? = ?^2 = (16+1)^2
+
+        // Math.pow의 결과는 double이기 때문에 캐스팅 필요
+        int result = (int) Math.pow( Math.pow(2, n) + 1, 2);
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
     }
 }
