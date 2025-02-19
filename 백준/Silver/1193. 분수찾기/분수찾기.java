cff972daee3251069f3c664c76758391a4e5e11c
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-02-20 03:48:51 +0900

    [Silver V] Title: 분수찾기, Time: 108 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,33 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
-class Main{
-	public static void main(String args[])throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int X=Integer.parseInt(br.readLine());
-		int n=0;
-		int top=1;
-		int bot=1;
-		int cnt=0;
-		if(X==1) {
-			System.out.println("1/1");
-		}else {
-			while(cnt<X){
-				n++;
-				cnt=n*(n+1)/2; //몇번 째 행인지 찾는 코드
-			}
-			int num=X-(n-1)*n/2;  //행의 몇번 째 값인지 찾는 코드
-			if(n%2==0) {  //짝수 행일 시 
-				top=num;
-				bot=n-num+1;
-			}else {
-				top=n-num+1;
-				bot=num;
-			}
-			System.out.println(top+"/"+bot);
-		}
-		
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        
+        // 대각선 개수가 1, 2, 3, .. -> X가 몇번째 라인의 몇번째 분수인지 알 수 있음
+
+        int line = 1;
+        while (X > line) {
+            X -= line++;
+        }
+
+        int lineSum = line + 1;  // 분자 + 분모
+        int a = 0;
+        int b = 0;
+
+        // 짝수 라인
+        if (line % 2 == 0) {
+            a = X;
+            b = lineSum - X;
+        }
+        // 홀수 라인
+        else {
+            b = X;
+            a = lineSum - X;
+        }
+
+        bw.write(String.format("%d/%d", a, b));
+        bw.flush();
+        bw.close();
+    }
 }
