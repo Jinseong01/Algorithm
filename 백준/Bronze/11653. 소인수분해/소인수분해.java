2eb54255ee3fb43d80a16826c63421c85843cf4f
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-02-23 03:07:47 +0900

    [Bronze I] Title: 소인수분해, Time: 120 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -11,11 +11,19 @@ public class Main {
             return;
         }
 
-        for ( int i = 2; i <= N; i++) {
+        // a * b = N
+        // if) a>루트N, b>루트N -> a*b > N
+        // 즉, a,b 중 1개는 루트 N보다 작다
+        // 루트N보다 큰 인수에 대해서는 루트N보다 작은 인수들의 조합으로 해결 가능하거나 안 나누어지는 것
+        for ( int i = 2; i <= Math.sqrt(N); i++) {
             while ( N % i == 0) {
                 bw.write(i + "\n");
                 N /= i;
             }
+        }
+
+        if (N>1) {
+            bw.write(N + "\n");
         }
 
         bw.flush();
