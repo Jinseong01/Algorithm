0ad9a6d3723a1325ba20ad57b7fe6c35c95e0414
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-02-25 22:58:32 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -2,9 +2,14 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
+        // MenOfPassion(A[], n) {
+        //    i = ⌊n / 2⌋;
+        //    return A[i]; # 코드1
+        // }
+        // 이기 때문에 실행횟수는 n과 상관없이 무조건 1번
+        // 따라서 최고차항 차수는 0
         bw.write(1 + "\n");
         bw.write(0 + "\n");
         bw.flush();
