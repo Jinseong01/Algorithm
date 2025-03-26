fc3853c44b03ea5219320c873f62e5cccb9b66e1
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-03-27 01:10:10 +0900

    [Silver III] Title: 소수 구하기, Time: 380 ms, Memory: 20708 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -24,18 +24,28 @@ public class Main {
     // 소수 판별
     private static boolean isPrime(int n) {
         // 0, 1
-        if (n < 2) {
+        if (n <= 1) {
             return false;
         }
 
-        // 소수가 아닌 경우
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        // 2
+        if (n == 2) {
+            return true;
+        }
+
+        // 2를 제외한 짝수는 소수가 아님
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        // 나머지 소수가 아닌 경우
+        for (int i = 3; i <= Math.sqrt(n); i+=2) {
             if (n % i == 0) {
                 return false;
             }
         }
 
-        // 소수인 경우
+        // 나머지 소수인 경우
         return true;
     }
 }
