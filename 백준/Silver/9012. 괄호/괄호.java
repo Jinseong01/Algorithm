a60efe70c3ce00f3f9ac434195e9623be1b55a4b
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-04-01 00:05:07 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean isValid = true;
+
+            for (char c : s.toCharArray()) {
+                if (c == '(') stack.push(c);
+                else {
+                    if (stack.isEmpty()) {
+                        isValid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+
+            if (!stack.isEmpty()) isValid = false;
+            bw.write(isValid ? "YES\n" : "NO\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
