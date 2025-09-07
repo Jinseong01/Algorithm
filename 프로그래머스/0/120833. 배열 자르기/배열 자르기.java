14bb6446be209a6996025b69c995d352478af585
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-07 18:22:49 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,11 +2,8 @@ class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = new int[num2-num1+1];
         
-        int idx=0;
-        for (int i=0; i<numbers.length; i++) {
-            if(num1<=i && i<=num2) {
-                answer[idx++] = numbers[i];
-            }
+        for (int i=num1; i<=num2; i++) {
+            answer[i-num1] = numbers[i];
         }
         
         return answer;
