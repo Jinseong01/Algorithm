fa7e7fbec5dac4a2ba991b9e3caca0fc75db0e9a
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-14 22:16:37 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.47 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        String[] tokens = s.split(" ");
+    
+        for (String token : tokens) {
+            if (token.equals("Z")) {
+                stack.pop();
+            }
+            else {
+                stack.push(Integer.parseInt(token));   
+            }
+        }
+        
+        int sum=0;
+        for (int i: stack) {
+            sum += i;
+        }
+        
+        return sum;
+    }
+}
