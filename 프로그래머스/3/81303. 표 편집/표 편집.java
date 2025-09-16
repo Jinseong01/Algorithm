8733651ef8946ab3fe503d13b829163263c979e3
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-16 22:06:49 +0900

    [level 3] Title: 표 편집, Time: 890.24 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        Stack<Integer> remove_order = new Stack<Integer>();
+        // 삭제된 행 관리
+        Stack<Integer> remove_order = new Stack<Integer>(); 
         
-        int table_size = n;
-        
+        // k는 현재 선택한 행의 번호
         for(int i=0; i<cmd.length; i++) {
             char c = cmd[i].charAt(0);
             
@@ -18,27 +18,29 @@ class Solution {
             else if(c=='U') {
                 k-=Integer.parseInt(cmd[i].substring(2));
             }
+            
             // 현재 행 삭제 후, 바로 아래 행 선택
             else if(c=='C') {
                 remove_order.add(k);
-                table_size--;
+                n--;
                 // 가장 마지막 행이었으면, 윗 행 선택
-                if(k==table_size) {
+                if(k==n) {
                     k--;   
                 }
             }
+            
             else if(c=='Z') {
                 // 복구한 행의 번호 <= 현재 행 번호
                 if(remove_order.pop()<=k) {
                     k++;
                 }
-                table_size++;
+                n++;
             }
         }
         
         
         StringBuilder builder = new StringBuilder();
-        for(int i=0; i<table_size; i++) {
+        for(int i=0; i<n; i++) {
             builder.append("O");            
         }
 
