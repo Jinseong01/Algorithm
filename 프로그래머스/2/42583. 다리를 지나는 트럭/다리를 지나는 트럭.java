a2afa30dd51324882407175acdeb1d4ac6948b71
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-18 02:34:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 24.08 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,48 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        int sum = 0;    // 다리 위 트럭 무게 총합
-		int time = 0;   // 총 시간 (다리에 오르고 내리는 작업이 동시에 발생하기 때문에, 시간 카운트는 최근에 다리에 올라간 트럭 기준)
+        ArrayDeque<Integer> bridge = new ArrayDeque<>();
+        int time = 0;
+        int totalWeight = 0;
+        int idx = 0;
 
-		for(int i = 0; i < truck_weights.length; i++) {
-            // 순서대로 각 트럭의 무게
-			int truck = truck_weights[i];
+        // 초기화
+        for(int i=0; i<bridge_length; i++) {
+            bridge.add(0);
+        }
 
-            // i번째 트럭이 다리에 올라가면 break
-			while(true) {
-				// 다리가 비었으면 트럭+1 & 시간+1
-				if(deque.isEmpty()) { 
-					deque.add(truck);
-					sum += truck;
-					time++;
-					break;
-				}
-                // 다리가 꽉 참
-                else if(deque.size() == bridge_length) {
-                    // 가장 앞에 트럭 나감
-					sum -= deque.poll();
-				}
-                // 다리 여유있음
-                else  {
-                    // 무게제한을 넘지 않는다면 트럭+1 & 시간+1
-					if(sum + truck <= weight) {
-						deque.add(truck);
-						sum += truck;
-						time++;
-						break;
-					}
-                    // 무게제한 넘는다면 기존 트럭을 1칸 이동 & 시간+1
-                    else {
-						deque.add(0);
-						time++;
-					}
-				}
-			}
-		}
+        while(!bridge.isEmpty()) {
+            // 1초마다 맨 앞 트럭 빠짐
+            totalWeight -= bridge.poll(); 
+            time++;
 
-        // 마지막 트럭이 올라가면 반복문 종료
-        // -> 해당 트럭이 다리 건너는거 계산
-		return time + bridge_length; 
+            if(idx < truck_weights.length) {
+                // 무게제한 넘지 않으면 트럭을 다리에 올림
+                if(totalWeight + truck_weights[idx] <= weight) {
+                    bridge.add(truck_weights[idx]);
+                    totalWeight += truck_weights[idx];
+                    idx++;
+                }
+                // 무게제한 넘으면 빈칸을 다리에 올림
+                else {
+                    bridge.add(0);
+                }
+            }
+        }
+
+        return time;
     }
 }
