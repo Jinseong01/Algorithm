812dd2aec0ff51f0f0bd47b9b1548bcc866461ad
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-20 01:56:34 +0900

    [level 2] Title: 기능개발, Time: 5.83 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -14,10 +14,10 @@ class Solution {
         // 2. 배포 순서를 계산
         ArrayDeque<Integer> deque = new ArrayDeque<>();
         int maxDay = days[0]; // 현재 배포 기준일 (첫번째 기능으로 초기화)
-        int count = 1;        // 현재 배포에 포함되는 기능 수 (첫번째 기능)
+        int count = 0;
 
-        // 3. 두 번째 기능부터 마지막 기능까지 확인
-        for (int i = 1; i < n; i++) {
+        // 3. 첫번째 기능부터 마지막 기능까지 확인하며, 배포 단위를 나눔
+        for (int i = 0; i < n; i++) {
             // 현재 기능의 예상 소요일 <= 이전 기능의 예상 소요일
             // 즉, 이전 기능과 함께 배포
             if (days[i] <= maxDay) {
@@ -32,7 +32,7 @@ class Solution {
             }
         }
 
-        // 4. 마지막 배포 단위 추가
+        // 4. 마지막 배포는 별도로 추가 필요
         deque.add(count);
 
         // 5. int[] 변환 후 반환
