25431e1529161b46e257b21c5d31d2ec3673ac31
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-09-28 02:28:51 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,14 +4,10 @@ class Solution {
     public int solution(String[][] clothes) {
         // 최소 1개 이상의 카테고리별 의상으로 구성된 조합 총 개수
         
-        // 1. {카테고리 : 옷 이름}
-        HashMap<String, List<String>> closet = new HashMap<>();
+        // 1. {카테고리 : 카테고리별 옷 개수}
+        HashMap<String, Integer> closet = new HashMap<>();
         for (String[] cloth : clothes) {
-            String name = cloth[0];
-            String category = cloth[1];
-            
-            closet.putIfAbsent(category, new ArrayList<>());
-            closet.get(category).add(name);
+            closet.put(cloth[1], closet.getOrDefault(cloth[1], 0) + 1);
         }
         
         // 2. 조합 개수 구하기
@@ -19,8 +15,8 @@ class Solution {
         // -> 카테고리별로 의상을 선택하지 않는거까지 +1하여 모든 경우의 수를 구함
         //    마지막에 아무것도 입지 않는 경우 -1해줌
         int answer = 1;
-        for(String category : closet.keySet()) {
-            answer *= closet.get(category).size()+1;
+        for(int count : closet.values()) {
+            answer *= count+1;
         }
         
         return answer-1;
