bfde92fea484717460cc1192e47efd8255c35a21
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2025-10-07 20:45:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,27 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         HashSet<String> relay = new HashSet<>();
-        
-        int idx = 0;
-        int round = 1;
-        
+
         relay.add(words[0]);
         char last = words[0].charAt(words[0].length()-1);
         
         for (int i = 1; i < words.length; i++) {
-            // 1. 사이클 순환
-            idx = (idx + 1) % n;
-            if (idx == 0) {
-                round++;
-            }
-
-            // 2. 규칙 확인
+            // 1. 규칙 확인
             String word = words[i];
             if (relay.contains(word) || word.charAt(0) != last) {
-                return new int[]{idx + 1, round};
+                int idx = (i % n) + 1;
+                int round = (i / n) + 1;
+                return new int[]{idx, round};
             }
 
-            // 3. 갱신
+            // 2. 갱신
             relay.add(word);
             last = word.charAt(word.length() - 1);
         }
