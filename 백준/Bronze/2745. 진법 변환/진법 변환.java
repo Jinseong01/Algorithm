b192e895fc257499c23c4ffffdd820b431cd73e3
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-11 15:44:33 +0900

    [Bronze II] Title: 진법 변환, Time: 92 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,44 +1,14 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        String s = st.nextToken();
-        int n = Integer.parseInt(st.nextToken());
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
 
-//        36진수일때, zzzzz
-//        z = 35 * 36^0
-//        zㅁ = 35 * 36^1
-//        zㅁㅁ = 35 * 36^2
-//        zㅁㅁㅁ = 35 * 36^3
-//        zㅁㅁㅁㅁ = 35 * 36^4
-
-        int num;
-        int result=0;
-        int idx = 0;
-        for(int i=s.length()-1; i>=0; i--){
-            char c = s.charAt(i);
-
-            // 숫자인 경우
-            if('0' <= c && c <= '9'){
-                num = c - '0';
-            }
-
-            // 문자인 경우
-            else {
-                num = c - 'A' + 10;
-            }
-
-            result += (int) (num * Math.pow(n, idx++));
-        }
-
-        bw.write(String.valueOf(result));
-        bw.flush();
-        br.close();
+        System.out.println(Long.parseLong(N, B));
     }
 }
