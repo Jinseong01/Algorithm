ef4739fa4dff6c60ac3af97619e70a06c7573ed0
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-13 14:34:52 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 92 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,31 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int up = Integer.parseInt(st.nextToken());
-        int down = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        // for문 -> 시간 초과 -> 다른 방식 필요
-        // day = K + 1
-        // 하루마다 up-down씩 올라감 + 마지막 날에는 up만큼 올라감 >= V
-        // K(up-down) + up >= V
-        // K >= (V-up) / (up-down)
+        // 마지막 날은 정상 도착하면 끝 -> 마지막 날 제외하고 계산
+        int height = V - A;
+        int move = A - B;
 
-        int K = (V-up) / (up-down);
-        if ((V - up) % (up - down) != 0) { // 나머지가 있으면
-            K++; // 하루 추가
-        }
+//        int daysBeforeLast = height / move;
+//        if (height % move != 0) {
+//            daysBeforeLast++;
+//        }
+        int daysBeforeLast = (height + (move-1)) / move;
 
-        int day = K + 1;
-
-        bw.write(day + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(daysBeforeLast + 1);
     }
 }
