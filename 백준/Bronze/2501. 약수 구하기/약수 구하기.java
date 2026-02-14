420436647e9dd7bfbd6a8111feaf3a03681c38e2
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-15 02:12:28 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,30 +1,38 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] input = br.readLine().split(" ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
-        int count = 0;
-        int result = 0;
+        List<Integer> divisors = new ArrayList<>();
 
-        for(int i = 1; i <= N; i++) {
-            if(N % i == 0) {
-                count++;
-            }
-            if(count == K) {
-                result = i;
-                break;
+        // 1. 루트N까지 탐색
+        int root = (int) Math.sqrt(N);
+        for (int i = 1; i <= root; i++) {
+            if (N % i == 0) {
+                divisors.add(i);
+
+                // 제곱수가 아닌 경우에는 그 쌍인 약수까지 저장
+                if (i != N / i) {
+                    divisors.add(N / i);
+                }
             }
         }
 
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
+        // 2. 약수들을 오름차순으로 정렬
+        Collections.sort(divisors);
+
+        // 3. K번째 약수 출력
+        if (divisors.size() >= K) {
+            System.out.println(divisors.get(K - 1));
+        }
+        else {
+            System.out.println(0);
+        }
     }
 }
