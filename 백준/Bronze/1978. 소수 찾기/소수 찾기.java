90c2cba9c96f3a527eddb12f304233786ed872cd
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-19 10:53:03 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -4,36 +4,34 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-
-            // 1은 패스
-            if (num == 1) {
-                continue;
-            }
-
-            // 소수여부
-            boolean flag = true;
-
-            for(int j = 2; j < num; j++) {
-                if (num % j == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if (flag) {
+            if (isPrime(num)) {
                 count++;
             }
         }
 
-        bw.write(count + "\n");
-        bw.flush();
-        br.close();
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
