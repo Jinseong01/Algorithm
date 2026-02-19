90b5e9fafb4b3f223ffaa3f87ba083496e1e4469
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-19 15:36:11 +0900

    [Bronze I] Title: 소인수분해, Time: 92 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,32 +1,30 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> list = new ArrayList<>();
 
         int N = Integer.parseInt(br.readLine());
 
-        if (N==1) {
+        if (N == 1) {
             return;
         }
 
-        // a * b = N
-        // if) a>루트N, b>루트N -> a*b > N
-        // 즉, a,b 중 1개는 루트 N보다 작다
-        // 루트N보다 큰 인수에 대해서는 루트N보다 작은 인수들의 조합으로 해결 가능하거나 안 나누어지는 것
-        for ( int i = 2; i <= Math.sqrt(N); i++) {
-            while ( N % i == 0) {
-                bw.write(i + "\n");
-                N /= i;
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                list.add(i);
+                N = N / i;
             }
         }
 
-        if (N>1) {
-            bw.write(N + "\n");
+        if (N > 1) {
+            list.add(N);
         }
 
-        bw.flush();
-        bw.close();
+        for (int num : list) {
+            System.out.println(num );
+        }
     }
 }
