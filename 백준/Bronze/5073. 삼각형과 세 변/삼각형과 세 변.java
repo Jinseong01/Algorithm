724af09cc654f96fa0fc74ebc416cdd2138569b0
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-21 02:27:58 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,41 +1,36 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         while (true) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if (a==0 && b==0 && c==0) {
-                break;
-            }
+            if (a == 0 && b == 0 && c == 0) break;
 
-            int max = Math.max(a, Math.max(b,c));
-            if (a+b+c-max <= max) {
-                bw.write("Invalid\n");
-                continue;
-            }
+            int max = Math.max(a, Math.max(b, c));
 
-            if(a==b && b==c) {
-                bw.write("Equilateral\n");
+            if (max >= a + b + c - max) {
+                sb.append("Invalid\n");
             }
-            else if( a==b || a==c || b==c ) {
-                bw.write("Isosceles\n");
+            else if (a == b && b == c) {
+                sb.append("Equilateral\n");
+            }
+            else if (a == b || b == c || a == c) {
+                sb.append("Isosceles\n");
             }
             else {
-                bw.write("Scalene\n");
+                sb.append("Scalene\n");
             }
         }
 
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
