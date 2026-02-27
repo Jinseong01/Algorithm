793b47f1906c6e1ee67bb062986bb55f77ace07b
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-02-28 00:55:36 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -3,29 +3,15 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // MenOfPassion(A[], n) {
-        //    sum <- 0;
-        //    for i <- 1 to n - 1
-        //        for j <- i + 1 to n
-        //            sum <- sum + A[i] × A[j]; # 코드1
-        //    return sum;
-        // }
-        // 이기 때문에 코드1의 실행횟수는
-        // i=1 -> j는 n-1번
-        // i=2 -> j는 n-2번
+        int n = Integer.parseInt(br.readLine());
+
+        // i=1 -> n-1
+        // i=2 -> n-2
         // ...
-        // i=n-1 -> j는 1번
-        // 총 = 1+2+...+n-1번 = (n-1)*n/2
-        // n(1 ≤ n ≤ 500,000)이기 때문에
-        // long으로 처리
+        // i=n-1 -> 1
 
-        long n = Long.parseLong(br.readLine());
-
-        bw.write((n-1)*n/2 + "\n");
-        bw.write("2\n");
-        bw.flush();
-        bw.close();
+        System.out.println((long) (n-1) * n / 2);
+        System.out.println(2);
     }
 }
