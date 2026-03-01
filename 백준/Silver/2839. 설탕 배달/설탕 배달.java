24ef3b61e0a0bab8570bff82f4ce961e003e8623
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-01 23:09:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,33 +1,29 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        // 설탕 봉지는 3kg, 5kg
+        // N킬로그램 배달해야 할 때, 최대한 적은 봉지 개수
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        while (N > 0 ) {
-            if (N < 3) {
-                count = -1;
-                break;
-            }
+        int five = N / 5;
+        while (five >= 0) {
+            int rest = N - (five * 5);
 
-            //5kg으로 해결
-            if (N % 5 == 0) {
-                count += N / 5;
-                break;
+            // 3으로 나누어떨어지면 바로 종료
+            if (rest % 3 == 0) {
+                System.out.println(five + rest / 3);
+                return;
             }
-            //5kg으로 해결 안되면
+            // 3으로 나누어떨어지지않으면 5kg 봉지를 -1개
             else {
-                N -= 3;
-                count++;
+                five--;
             }
         }
 
-        bw.write(count + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(-1);
     }
 }
