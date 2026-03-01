55a9b35b722623330cfa481b1ba6780e8c557afc
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-01 22:55:47 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 86684 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,24 +2,24 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 종말의 수란 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수
+        // ex) 666, 1666, 2666, ...
+        // N번째 종말의 수 구하기
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        // 종말의 수가 커지면 6660 ~ 6666 같은 구간이 생기기 때문에 규칙으로 하기는 어렵다
 
         int count = 1;
-        int number = 666;
+        int num = 666;
+        while (count != N) {
+            num++;
 
-        while(count != N) {
-            number++;
-            if(String.valueOf(number).contains("666")) {
+            if (String.valueOf(num).contains("666")) {
                 count++;
             }
         }
-        
-        bw.write(String.valueOf(number));
-        bw.flush();
-        bw.close();
+
+        System.out.println(num );
     }
 }
