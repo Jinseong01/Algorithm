4b3d32b4f5838371afd1a3c6407ad9bbea8e008a
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-01 18:20:43 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,63 +1,49 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // M×N 크기의 보드를 잘라서 8×8 크기의 체스판을 만듦
+        // 검흰이 알맞게 배치되기 위해 색칠해야 하는 정사각형의 최소 개수
 
-        int N  = Integer.parseInt(st.nextToken());
-        int M  = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(" ");
+        int N = Integer.parseInt(line[0]);
+        int M = Integer.parseInt(line[1]);
 
         char[][] board = new char[N][M];
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                board[i][j] = line.charAt(j);
-            }
+            board[i] = br.readLine().toCharArray();
         }
 
-        // 보드 크기에 따라 8x8의 경우가 다양함
-        // -> 이때 가장 좌측 상단 시작 사각형의 좌표가 있어야 8x8체스판 체크 편함
-        // N,M이 8,8이면 (1,1)
-        // N,M이 10,10이면 (1,1) (1,2) (1,3) ~ (3,1) (3,2) (3,3)
-        // N,M이 11,12이면 (1,1) (1,2) (1,3) (1,4) (1,5) ~ (4,1) (4,2) (4,3) (4,4) (4,5)
-        // 즉, 가장 좌측 상단 사각형의 좌표는 (N-7, M-7)까지 가능
-        // 좌표니까 -1로 고려해서 진행
-
-        int min = 64;
-        for(int i = 0; i < N-7; i++) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
-                // 기존 min과 check를 통한 min 중 최소값 선택
-                min = Math.min(min, check(i, j, board));
+                min = Math.min(min, checkMin(i, j, board));
             }
         }
 
-        bw.write(min + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(min);
     }
 
-    // i,j부터 시작하여 8x8체스판을 위해 바꾸는 사각형 수 계산 함수
-    // 흰색 시작 vs 검정색 시작 중 최소값 반환
-    private static int check(int i, int j, char[][] board) {
+    private static int checkMin(int startX, int startY, char[][] board) {
         int count = 0;
 
-        // for문은 흰색 시작이라 가정하고 진행
         char currentColor = 'W';
-        for (int k = i; k < i+8; k++) {
-            for (int l = j; l < j+8; l++) {
-                if (board[k][l] != currentColor) {
+        for (int i = startX; i < startX + 8; i++) {
+            for (int j = startY; j < startY + 8; j++) {
+                if (board[i][j] != currentColor) {
                     count++;
                 }
-                if(currentColor == 'W') {
+
+                if (currentColor == 'W') {
                     currentColor = 'B';
                 }
                 else {
                     currentColor = 'W';
                 }
             }
+
             if (currentColor == 'W') {
                 currentColor = 'B';
             }
@@ -66,6 +52,6 @@ public class Main {
             }
         }
 
-        return Math.min(count, 64-count);
+        return Math.min(count, 64 - count);
     }
 }
