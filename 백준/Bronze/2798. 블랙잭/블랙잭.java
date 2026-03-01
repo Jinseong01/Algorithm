65ceab16c6cd082db82033b967dcbc0b743f0f03
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-01 17:56:05 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,41 +1,73 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // M에 최대한 가까운 카드 3장의 합
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = 0 ;
-        for(int i = 0 ; i < n-2 ; i++){
-            for(int j = i+1 ; j < n-1 ; j++){
-                for (int k = j + 1; k < n; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];
+        Arrays.sort(arr);
 
-                    if (sum == m) {
-                        result = sum;
-                        break;
-                    }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
 
-                    if (result < sum && sum < m) {
-                        result = sum;
-                    }
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                // M보다 크면
+                if (sum > M) {
+                    right--;
+                }
+                // M 이하이면
+                else {
+                    max = Math.max(max, sum);
+                    left++;
                 }
             }
         }
 
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
+        System.out.println(max);
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        // M에 최대한 가까운 카드 3장의 합
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+//
+//        st = new StringTokenizer(br.readLine());
+//        int[] arr = new int[N];
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0; i < N - 2; i++) {
+//            for (int j = i + 1; j < N - 1; j++) {
+//                for (int k = j + 1; k < N; k++) {
+//                    if (arr[i] + arr[j] + arr[k] <= M) {
+//                        max = Math.max(max, arr[i] + arr[j] + arr[k]);
+//                    }
+//                }
+//            }
+//        }
+//
+//        System.out.println(max);
+//    }
+//}
