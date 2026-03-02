9ebe3b6372ebe61acf3f73f572e2192f6b6cf16e
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-02 16:34:17 +0900

    [Bronze II] Title: 커트라인, Time: 108 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,27 +1,21 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int k  = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-
-        bw.write(arr[N-k] + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(arr[N - k]);
     }
 }
