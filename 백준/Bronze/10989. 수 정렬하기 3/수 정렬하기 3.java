b823c1895d09ce77650130969d487261f8756cf3
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-02 22:49:22 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1544 ms, Memory: 336652 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,18 +1,16 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        // 입력되는 수가 10,000보다 작거나 같은 자연수이기 때문에
-        // 인덱스 계산을 쉽게 하기 위해 +1해서 생각
-        int[] count = new int[10001];
 
+        // 1  ~ 10,000
+        int[] count = new int[10000 + 1];
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            count[num]++;
+            count[Integer.parseInt(br.readLine())]++;
         }
 
         StringBuilder sb = new StringBuilder();
@@ -23,8 +21,6 @@ public class Main {
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
