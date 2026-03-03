f3f50eaf74c6549736db637fe7f3580169d5fcbd
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-03 23:53:42 +0900

    [Silver II] Title: 좌표 압축, Time: 1680 ms, Memory: 251832 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -4,41 +4,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
         int[] origin = new int[N];
-        int[] copy = new int[N];
+        int[] sorted = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             origin[i] = Integer.parseInt(st.nextToken());
-            copy[i]= origin[i];
+            sorted[i] = origin[i];
         }
 
-        // 크기대로 정렬  (ex. -10 -9 2 4 4)
-        Arrays.sort(copy);
+        // 정렬
+        Arrays.sort(sorted);
 
-        // 크기 비교해서 카운트 & 중복 판단 필요
-        // -> Map에 중복 고려하여 idx를 같이 넣음으로써, 크기 비교해서 카운트까지 해결
-        // (idx가 자신보다 작은 숫자 개수)
-        Map<Integer, Integer> map = new HashMap<>();
-        int idx = 0;
-        for(int i : copy) {
-            if(!map.containsKey(i)) {
-                map.put(i, idx++);
+        // 중복 제거
+        int[] unique = new int[N];
+        int size = 0;
+        for (int i = 0; i < N; i++) {
+            if (size == 0 || sorted[i] != sorted[i - 1]) {
+                unique[size++] = sorted[i];
             }
         }
 
-        // 결과 출력 시에는 origin으로 중복되는 요소 반복 출력
-        StringBuilder sb = new StringBuilder();
-        for (int i : origin) {
-            sb.append(map.get(i)).append(" ");
+        // 값-순위 매핑
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            map.put(unique[i], i);
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        StringBuilder sb = new StringBuilder();
+        for (int num : origin) {
+            sb.append(map.get(num)).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
