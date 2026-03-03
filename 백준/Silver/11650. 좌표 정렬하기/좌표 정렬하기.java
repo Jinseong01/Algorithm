e2032790e4fef085a57f5cf50dbe4be9149b9d6c
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-03 19:35:56 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 640 ms, Memory: 50632 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,32 +1,25 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        
+
         int[][] arr = new int[N][2];
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // 2차원 배열 정렬
-        Arrays.sort(arr, (e1, e2) -> {
-            if (e1[0] == e2[0]) {
-                return e1[1] - e2[1];
-                // 양수면 e1[1]이 더 크니까 e1[1]가 뒤
-                // 음수면 e2[1]이 더 크니까 e2[1]가 뒤
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
             }
-            else {
-                return e1[0] - e2[0];
-            }
+            
+            return a[0] - b[0];
         });
 
         StringBuilder sb = new StringBuilder();
@@ -34,9 +27,6 @@ public class Main {
             sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
