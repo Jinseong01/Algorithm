f57834fa788f5dc3cd9b86a0eacf75bbe4080945
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-03 19:48:49 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 27348 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,43 +1,60 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[N];
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+            set.add(br.readLine());
         }
 
+        List<String> list = new ArrayList<>(set);
+        list.sort((a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            }
 
-        Arrays.sort(arr, (str1, str2) -> {
-            if(str1.length() == str2.length()) {
-                return str1.compareTo(str2);
-                // compareTo가 객체 간의 크기를 비교
-                // String일 때에는 사전순서 비교
-            }
-            else {
-                return str1.length() - str2.length();
-            }
+            return a.length() - b.length();
         });
 
-        Set<String> set = new LinkedHashSet<>(Arrays.asList(arr));
-        // HashSet은 순서 보장 X
-
         StringBuilder sb = new StringBuilder();
-        for (String s : set) {
-            sb.append(s).append("\n");
+        for (String s : list) {
+            sb.append(s).append('\n');
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//
+//        TreeSet<String> set = new TreeSet<>(
+//                (a, b) -> {
+//                    if (a.length() == b.length()) {
+//                        return a.compareTo(b);
+//                    }
+//
+//                    return a.length() - b.length();
+//                }
+//        );
+//
+//        for (int i = 0; i < N; i++) {
+//            set.add(br.readLine());
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (String s : set) {
+//            sb.append(s).append('\n');
+//        }
+//
+//        System.out.println(sb);
+//    }
+//}
