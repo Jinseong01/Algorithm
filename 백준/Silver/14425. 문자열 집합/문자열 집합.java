33b36eaec22721ea693a3cd865876700fdf6bdd3
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-04 13:15:19 +0900

    [Silver IV] Title: 문자열 집합, Time: 356 ms, Memory: 31148 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,34 +1,29 @@
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Set<String> arr1 = new HashSet<>();
-
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            arr1.add(br.readLine());
+            set.add(br.readLine());
         }
 
         int count = 0;
-        for (int i = 0; i < M; i++){
-            if (arr1.contains(br.readLine())) {
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+
+            if (set.contains(s)) {
                 count++;
             }
         }
 
-        bw.write(count + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(count);
     }
 }
