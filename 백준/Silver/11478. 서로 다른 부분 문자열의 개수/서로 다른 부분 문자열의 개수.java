28d658d73cec2705883d6a3fa98ecf9cdd2d6af2
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-09 15:57:57 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 684 ms, Memory: 226824 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -4,21 +4,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String S = br.readLine();
+        String s = br.readLine();
 
         Set<String> set = new HashSet<>();
-
-        for(int i = 0; i < S.length(); i++) {
-            for(int j = i+1; j <= S.length(); j++) {
-                set.add(S.substring(i, j)); //substring i부터 j 직전까지의 문자열
+        for(int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                set.add(s.substring(i, j));
             }
         }
 
-        bw.write(String.valueOf(set.size()));
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(set.size());
     }
 }
+
+
+//public class Main {
+//
+//    static String s;
+//    static Set<String> set;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        s = br.readLine();
+//        set = new HashSet<>();
+//        for(int i = 0; i < s.length(); i++) {
+//            dfs(i, "");
+//        }
+//
+//        System.out.println(set.size());
+//    }
+//
+//    private static void dfs(int idx, String current) {
+//        if (idx == s.length()) {
+//            return;
+//        }
+//
+//        current += s.charAt(idx);
+//        set.add(current);
+//
+//        dfs(idx + 1, current);
+//    }
+//}
