83d2547f0d8fbba4db23b3a749570d1976981cc1
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-10 16:04:41 +0900

    [Bronze I] Title: 최소공배수, Time: 116 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,41 +1,40 @@
 import java.io.*;
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // A와 B의 최소공배수
+        // (A × B) = LCM(A, B) × GCD(A, B)
+        // LCM(A, B) = (A * B) / GCD(A, B)
+        // GCD(A, B) = GCD(B, A % B)
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            // 최소공배수 = 두 수의 곱 / 최대공약수
-            // 유클리드 호제법 이용
-            // A, B의 최대공약수 = B, (A%B)의 최대 공약수 = ...
-            int lcm = A * B / gcd(A, B);
-            sb.append(lcm).append("\n");
+            int res = (A * B) / GCD(A, B);
+//            int res = (A * B) / BigInteger.valueOf(A).gcd(BigInteger.valueOf(B)).intValue();
+
+
+            sb.append(res).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 
-
-    private static int gcd(int a, int b) {
-        if (b == 0) {
+    private static int GCD(int a, int b) {
+        if(b == 0) {
             return a;
         }
-        else {
-            return gcd(b, a % b);
-        }
+
+        return GCD(b, a % b);
     }
 }
