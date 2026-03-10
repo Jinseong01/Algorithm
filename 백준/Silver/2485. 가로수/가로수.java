8cbbb4e7aa46f381fc406c88f266489660f2c9db
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-11 01:10:18 +0900

    [Silver IV] Title: 가로수, Time: 220 ms, Memory: 22832 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,39 +1,38 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+
+        int[] trees = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            trees[i] = Integer.parseInt(br.readLine());
         }
 
-        // 기존 나무 간격들의 최대공약수로 나무를 심으면 전체적으로 균일하게 심어짐
+        // 나무 간격들의 최대공약수 계산
         int gcd = 0;
-        for (int i = 0; i < N-1; i++) {
-            int distance = arr[i+1] - arr[i];
-            gcd = gcd(distance, gcd);
+        for (int i = 1; i < N; i++) {
+            int dist = trees[i] - trees[i - 1];
+            gcd = GCD(gcd, dist);
         }
 
-        int total = arr[N-1] - arr[0];
-        // (total/gcd)는 간격의 개수이기 때문에 +1해야 나무의 개수
-        // 마지막으로 기존 나무들의 개수는 제외
-        bw.write((total/gcd)+1-arr.length + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        // 추가로 심어야하는 나무 개수 계산
+        int answer = 0;
+        for (int i = 1; i < N; i++) {
+            int dist = trees[i] - trees[i - 1];
+            answer += (dist / gcd) - 1;
+        }
+
+        System.out.println(answer);
     }
 
-    private static int gcd(int a, int b) {
+    private static int GCD(int a, int b) {
         if (b == 0) {
             return a;
         }
-        else {
-            return gcd(b, a % b);
-        }
+
+        return GCD(b, a % b);
     }
 }
