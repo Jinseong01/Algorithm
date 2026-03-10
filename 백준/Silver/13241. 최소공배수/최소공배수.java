fafb7f4ed7bd44317cd075896670de78097c27b9
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-11 00:42:33 +0900

    [Silver V] Title: 최소공배수, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.java
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -2,32 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long A = Long.parseLong(st.nextToken());
         long B = Long.parseLong(st.nextToken());
 
-        // 최소공배수 = 두 수의 곱 / 최대공약수
-        // 유클리드 호제법 이용
-        // A, B의 최대공약수 = B, (A%B)의 최대 공약수 = ...
-        long lcm = A * B / gcd(A, B);
-
-        bw.write(lcm + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(LCM(A, B));
     }
 
-    private static long gcd(long a, long b) {
+    private static long LCM(long a, long b) {
+        return (a * b) / GCD(a, b);
+    }
+
+    private static long GCD(long a, long b) {
         if (b == 0) {
             return a;
         }
-        else {
-            return gcd(b, a % b);
-        }
+        return GCD(b, a % b);
     }
 }
