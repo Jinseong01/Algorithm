deebc3362cd899e9410d280279c0a923789c222d
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-12 00:28:30 +0900

    [Silver II] Title: 베르트랑 공준, Time: 792 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,55 +1,41 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        // n보다 크고, 2n보다 작거나 같은 소수의 개수
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
         while (true) {
             int n = Integer.parseInt(br.readLine());
 
-            if (n == 0) {
-                break;
-            }
+            if (n == 0) break;
 
-            int count = 0;
-            for (int i = n + 1; i <= 2 * n; i++) {
-                if (isPrime(i)) {
-                    count++;
-                }
+            count = 0;
+            for (int j = n + 1; j <= 2 * n; j++) {
+                isPrime(j);
             }
-
-            bw.write(count + "\n");
+            sb.append(count).append("\n");
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 
-    private static boolean isPrime(int i) {
-        // 1
-        if (i == 1) {
-            return false;
+    private static void isPrime(int a) {
+        if (a <= 1) {
+            return;
         }
 
-        // 2
-        if (i == 2) {
-            return true;
-        }
-
-        // 나머지 짝수
-        if (i % 2 == 0) {
-            return false;
-        }
-
-        // 나머지 소수가 아닌 경우
-        for (int j = 3; j <= Math.sqrt(i); j++) {
-            if (i % j == 0) {
-                return false;
+        for (int i = 2; i * i <= a; i++) {
+            if (a % i == 0) {
+                return;
             }
         }
 
-        return true;
+        count++;
     }
 }
