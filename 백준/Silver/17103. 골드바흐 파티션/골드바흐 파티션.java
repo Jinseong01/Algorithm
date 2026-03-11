8dd6d15f903279f932b91735e8886dbb1007954a
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-12 00:41:48 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 232 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,49 +1,48 @@
 import java.io.*;
 
 public class Main {
+
     static final int MAX = 1000000;
-    static boolean[] sosu = new boolean[MAX+1];
+    static boolean[] prime = new boolean[MAX + 1];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 짝수 N이 주어졌을 때, 골드바흐 파티션의 개수
+        // 골드바흐 파티션 : 짝수 N을 두 소수의 합으로 나타내는 표현 (두 소수의 순서만 다른 것은 같은 파티션)
 
-        init();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sieve();
 
         int T = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
 
-            int partition = 0;
+            int count = 0;
             for (int j = 2; j <= N / 2; j++) {
-                if (sosu[j] && sosu[N - j]) {
-                    partition++;
+                if (prime[j] && prime[N - j]) {
+                    count++;
                 }
             }
 
-            bw.write(partition + "\n");
+            sb.append(count).append("\n");
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 
-    public static void init() {
-        sosu[0] = sosu[1] = false;
-
-        // 나머지 모든 수를 소수로 가정
+    private static void sieve() {
+        // 초기화
         for (int i = 2; i <= MAX; i++) {
-            sosu[i] = true;
+            prime[i] = true;
         }
 
-        // 에라토스테네스의 체
-        // 해당 수가 소수이면, 그 수의 배수는 소수가 X
         for (int i = 2; i * i <= MAX; i++) {
-            if (sosu[i]) {
+            // i가 소수라면 i의 배수는 소수가 아님
+            if (prime[i]) {
+                // 이전 단계를 고려하여 i*i부터 배수 확인
                 for (int j = i * i; j <= MAX; j += i) {
-                    sosu[j] = false;
+                    prime[j] = false;
                 }
             }
         }
