117be833a9e800332ea43a91a48bd78b86451436
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-17 01:22:24 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 15720 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -4,31 +4,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> queue = new ArrayDeque<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N =  Integer.parseInt(st.nextToken());
+        int K =  Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        Queue<Integer> q = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            q.add(i);
+            queue.add(i);
         }
 
-        bw.write("<");
-
-        while (q.size() > 1) {
-            for (int i = 0; i < K-1; i++) {
-                int tmp = q.poll();
-                q.add(tmp);
+        sb.append("<");
+        while (queue.size() > 1) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.add(queue.poll());
             }
-            bw.write(q.poll() + ", ");
+            sb.append(queue.poll()).append(", ");
         }
-        bw.write(q.poll() + ">");
+        sb.append(queue.poll()).append(">");
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 }
