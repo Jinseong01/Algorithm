7bacf3a88b9123876830a4d698fc0a6519b12d1d
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-18 01:03:08 +0900

    [Bronze IV] Title: 녹색거탑, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.java
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -1,16 +1,11 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        // 1 -> 1
-        // 2 -> 2*2
-        // 3 -> 2*2*2
-
-        System.out.println((int)Math.pow(2, N));
+        System.out.println((int) Math.pow(2, N));
     }
 }
