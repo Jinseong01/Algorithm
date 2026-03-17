2c3417b87a89f040656d9f7aa0dc087ad307c48e
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-18 00:53:26 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 140 ms, Memory: 15188 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -5,46 +5,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        Deque<int []> queue = new ArrayDeque<>();
 
         int N = Integer.parseInt(br.readLine());
 
-        Deque<int []> deque = new ArrayDeque<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            // [번호, 이동값] 저장
-            deque.add(new int[]{i + 1, Integer.parseInt(st.nextToken())});
+        for (int i = 1; i <= N; i++) {
+            queue.add(new int[]{i, Integer.parseInt(st.nextToken())});
         }
 
-        while (!deque.isEmpty()) {
-            // 선택은 맨 앞에 있는 원소로 진행
-            int[] cur = deque.pollFirst();
-
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
             sb.append(cur[0]).append(" ");
+
+            if (queue.isEmpty()) break;
 
             int move = cur[1];
 
-            if (deque.isEmpty()) {
-                break;
-            }
-
-            // 일렬로 생각 (선택은 무조건 맨 앞에 있는거)
-            // A, B, C, D
-
-            // 우측 이동해서 선택 (= 앞에 있는걸 뒤로 옮겨야함)
-            // 앞의 원소를 뺴서 뒤로 배치
             if (move > 0) {
-                // poll하고 난 뒤에 카운트하는 거기 때문에 -1 해주어야 함
-                for (int i = 0; i < move - 1; i++) {
-                    deque.addLast(deque.pollFirst());
+                move = (move-1) % queue.size();
+                for (int i = 0; i < move; i++) {
+                    queue.add(queue.poll());
                 }
             }
-            // 좌측 이동해서 선택 (= 뒤에 있는걸 앞으로 옮겨야함)
-            // 뒤의 원소를 뺴서 앞으로 배치
             else {
-                // 이미 poll한 거와 상관없음
-                for (int i = 0; i < Math.abs(move); i++) {
-                    deque.addFirst(deque.pollLast());
+                move = (-move) % queue.size();
+                for (int i = 0; i < move; i++) {
+                    queue.addFirst(queue.pollLast());
                 }
             }
         }
