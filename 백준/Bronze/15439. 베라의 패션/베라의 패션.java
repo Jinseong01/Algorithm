05acad3474d413a20db209599103045005e3723a
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-18 01:03:56 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -2,6 +2,8 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 상의 N벌과 하의 N벌
+        // 상의와 하의가 서로 다른 색상인 조합 개수
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
