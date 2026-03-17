c5ccb0083d681045f173fef08da3758c3f3f1866
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-18 01:07:20 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         System.out.println(factorial(N));
     }
@@ -13,8 +13,7 @@ public class Main {
         if (n <= 1) {
             return 1;
         }
-        else {
-            return n * factorial(n - 1);
-        }
+
+        return  n * factorial(n - 1);
     }
 }
