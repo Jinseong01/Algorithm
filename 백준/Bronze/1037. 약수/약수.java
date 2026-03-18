4395184f89ad669974ffb263deb9285ac3235a8c
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-19 00:22:01 +0900

    [Bronze I] Title: 약수, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -3,18 +3,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        // N의 1과 N을 제외한 모든 약수가 주어질 때, N 구하기
 
-        int num = Integer.parseInt(br.readLine());
-        int[] arr = new int[num];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < num; i++) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         Arrays.sort(arr);
 
-        System.out.println(arr[0] * arr[num - 1]);
+        System.out.println(arr[0] * arr[N - 1]);
     }
 }
