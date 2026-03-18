7c63b24a57cef64e8e555ee7a26fef316a4bc929
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-19 01:26:18 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 744 ms, Memory: 37748 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -4,47 +4,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Map<String, Integer> map = new HashMap<>();
+        List<String> list;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            String word = br.readLine();
-            if (word.length() >= M) {
-                map.put(word, map.getOrDefault(word, 0) + 1);
+            String s = br.readLine();
+            if (s.length() >= M) {
+                map.put(s, map.getOrDefault(s, 0) + 1);
             }
         }
 
-        List<String> words = new ArrayList<>(map.keySet());
-        words.sort((a, b) -> {
-            // 자주 나오든 단어가 앞
-            if (Integer.compare(map.get(b), map.get(a)) != 0) {
-                // map.get(b) > map.get(a)이면
-                // 양수 반환 -> (a,b)에서 (b,a)로 순서바뀜
-                return Integer.compare(map.get(b), map.get(a));
+        list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> {
+            // 자주 나오는 단어일수록 앞에 배치
+            if (!map.get(a).equals(map.get(b))) {
+                return map.get(b) - map.get(a);
             }
 
-            // 단어의 길이가 길수록 앞
-            if (b.length() != a.length()) {
-                // b.길이 > a.길이 이면
-                // 양수 반환 -> (a,b)에서 (b,a)로 순서바뀜
+            // 해당 단어의 길이가 길수록 앞에 배치
+            if (a.length() != b.length()) {
                 return b.length() - a.length();
             }
 
-            // 사전순 먼저일수록 앞
-            // a의 사전순서가 b의 사전순서보다 뒤이면
-            // 양수 반환 -> (a,b)에서 (b,a)로 순서바뀜
+            // 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치
             return a.compareTo(b);
         });
 
-        StringBuilder sb = new StringBuilder();
-        for (String word : words) {
-            sb.append(word).append("\n");
+        for (String s : list) {
+            sb.append(s).append("\n");
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
