99e002c4f33f642845a454be3e326e77bce2e6b6
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-19 00:48:56 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 236 ms, Memory: 25492 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -4,29 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> set = new HashSet<>();
 
         int N = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        Set<String> set = new HashSet<>();
-
+       int answer = 0;
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
 
-            // 누군가 새로 입장
             if (s.equals("ENTER")) {
                 set.clear();
             }
-            // 입장 아니고 채팅인사한 경우
             else {
-                // 기존에 채팅 안 친 경우만
                 if (!set.contains(s)) {
                     set.add(s);
-                    count++; // 누군가 새로 들어오면 clear()해주기 때문에 새로 들어오고 채팅친 경우도 카운트됨
+                    answer++;
                 }
             }
         }
 
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
