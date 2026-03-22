a365ac471df4cc5a566b45af6b7ebf387c2b6eec
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-23 00:18:16 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 232 ms, Memory: 30672 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -2,35 +2,36 @@ import java.io.*;
 
 public class Main {
 
-    static int count;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < T; i++) {
             String s = br.readLine();
-            count = 0;
-            int result = recursion(s, 0, s.length() - 1);
-            sb.append(result).append(" ").append(count).append("\n");
+
+            int[] res = isPalindrome(s, 0, s.length()-1);
+
+            sb.append(res[0]).append(" ").append(res[1]).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    public static int recursion(String s, int l, int r) {
-        count++;
-        if (l >= r) {
-            return 1;
+    private static int[] isPalindrome(String s, int l, int r) {
+        // 문자열 길이가 0 or 1
+        if (l>=r) {
+            return new int[]{1, 1};
         }
-        else if (s.charAt(l) != s.charAt(r)) {
-            return 0;
+
+        // 양 끝 문자가 다름
+        if (s.charAt(l) != s.charAt(r)) {
+            return new int[]{0, 1};
         }
-        else {
-            return recursion(s, l + 1, r - 1);
-        }
+
+        // 양 끝 문자가 같으면 안쪽 재귀호출
+        int[] res = isPalindrome(s, l+1, r-1);
+
+        return new int[]{res[0], res[1]+1};
     }
 }
