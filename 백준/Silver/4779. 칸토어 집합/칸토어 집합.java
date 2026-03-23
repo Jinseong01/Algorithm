f4a1b18b9978b595828745b49969ac161cfaf453
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-23 17:39:36 +0900

    [Silver III] Title: 칸토어 집합, Time: 216 ms, Memory: 24552 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,24 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static char[] result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         String line;
-        while ((line = br.readLine()) != null && !line.isEmpty()) {
-            int N = Integer.parseInt(line.trim());
+        while (true) {
+            line = br.readLine();
+            if (line == null) {
+                break;
+            }
+
+            int N = Integer.parseInt(line);
             int length = (int) Math.pow(3, N);
-            char[] arr = new char[length];
-            for (int i = 0; i < length; i++) arr[i] = '-';
-            buildCantor(arr, 0, length);
-            System.out.println(new String(arr));
+            result = new char[length];
+
+            Arrays.fill(result, '-');
+
+            cantor(0, length-1);
+
+            sb.append(String.valueOf(result)).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    private static void buildCantor(char[] arr, int start, int len) {
-        if (len < 3) return;
-        int t = len / 3;
-        for (int i = start + t; i < start + 2 * t; i++) arr[i] = ' ';
-        buildCantor(arr, start, t);
-        buildCantor(arr, start + 2 * t, t);
+    static void cantor(int left, int right) {
+        if (left == right) {
+            return;
+        }
+
+        int size = (right-left+1) / 3;
+        int midStart = left + size;
+        int midEnd = midStart + size - 1;
+
+        cantor(left, midStart-1);
+
+        for (int i=midStart; i<=midEnd; i++) {
+            result[i] = ' ';
+        }
+
+        cantor(midEnd+1, right);
     }
 }
