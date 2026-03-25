a82788ff3772580000ae74065d097ef41848fa34
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-25 15:19:00 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22196 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,51 +3,47 @@ import java.util.*;
 
 public class Main {
 
-    static int[] sequence;
     static boolean[] visited;
-    static StringBuilder sb;
+    static int[] arr;
+    static StringBuilder sb =  new StringBuilder();
 
+    public static void main(String[] args) throws IOException {
+        // 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 
-    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 수열을 담을 배열
-        sequence = new int[M];
+        visited = new boolean[N + 1]; // 1-based
+        arr = new int[M];
 
-        // 사용된 숫자를 체크할 배열 (1을 사용했으면 visited[1]로 할거기 때문에 N+1)
-        visited = new boolean[N + 1];
-
-        // DFS 함수
         dfs(0, N, M);
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    private static void dfs(int depth, int n, int m) {
-        // 수열의 길이가 m이 되면 끝
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(sequence[i]).append(" ");
+    private static void dfs(int depth, int N, int M) {
+        // 길이 M 수열 완성
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        // 1부터 n까지 탐색
-        for (int i = 1; i <= n; i++) {
-            // 아직 선택되지 않았다면
+        for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
-                visited[i] = true; // 선택
-                sequence[depth] = i; // 수열에 추가
-                dfs(depth + 1, n, m); // 해당 수열의 다음 숫자
+                // 현재 depth의 숫자 선택
+                visited[i] = true;
+                arr[depth] = i;
 
-                // 백트래킹
-                // 호출한 재귀함수가 끝난 뒤, visited[i]를 되돌림
+                // 다음 depth의 숫자 선택
+                dfs(depth + 1, N, M);
+
+                // 현재 depth 숫자 선택 취소
                 visited[i] = false;
             }
         }
