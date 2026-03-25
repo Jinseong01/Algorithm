20b68bcfd536c25d1a674fa6859ecb480a4a5b49
Jinseong01 <147074506+Jinseong01@users.noreply.github.com>
2026-03-25 15:09:15 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 576 ms, Memory: 48868 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -2,33 +2,35 @@ import java.io.*;
 
 public class Main {
 
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        int count = hanoi(N, 1, 2, 3, sb);
+        // n개를 from->to로 이동 (tmp 이용)
+        int count = hanoi(N, 1, 3, 2);
 
         System.out.println(count);
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    private static int hanoi(int N, int from, int tmp, int to, StringBuilder sb) {
-        // 옮기는 판이 1개 뿐이라면, 곧바로 이동
-        if (N == 1) {
+    private static int hanoi(int n, int from, int to, int tmp) {
+        // 원판 1개 -> 바로 이동
+        if (n == 1) {
             sb.append(from).append(" ").append(to).append("\n");
             return 1;
         }
 
-        // 가장 큰 원판 위의 (N-1)개를 from -> tmp로 이동
-        int before = hanoi(N-1, from, to, tmp, sb);
+        // 위에서 (n-1)개 치움
+        int before = hanoi(n - 1, from, tmp, to);
 
-        // 가장 큰 원판을 from -> to로 이동
+        // 큰 원판 이동
         sb.append(from).append(" ").append(to).append("\n");
 
-        // tmp로 옮겼던 (N-1)개를 tmp -> to로 이동
-        int after = hanoi(N-1, tmp, from, to, sb);
+        // 치웠던 (n-1)개 이동
+        int after = hanoi(n - 1, tmp, to, from);
 
         return before + 1 + after;
     }
